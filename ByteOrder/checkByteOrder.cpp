bc8a6aca8ca997547ef8f1fb1db3e6e612b3b6f7
Choi Hae Seong <39898936+chs0218@users.noreply.github.com>
2022-09-13 23:46:09 +0900

    과제 완료

--- ByteOrder/checkByteOrder.cpp
+++ ByteOrder/checkByteOrder.cpp
@@ -9,33 +9,26 @@ bool IsBigEndian();
 
 bool IsLittleEndian()
 {
-	u_short s1 = 0x1234;
-	u_short s2 = htons(s1);
-
-	if (s1 != s2)
-		return true;
-	return false;
+    u_short us = 0x0001;
+    char* p = (char*)&us;
+    return p[0];
 }
 
 bool IsBigEndian()
 {
-	u_short s1 = 0x1234;
-	u_short s2 = htons(s1);
-
-	if (s1 = s2)
-		return true;
-	return false;
+    u_short us = 0x0100;
+    char* p = (char*)&us;
+    return p[0];
 }
 
-int main(int argc, char* argv[])
-{
-	WSADATA wsa;
-	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
-		return 1;
+int main() {
+    if (IsLittleEndian())
+        printf("I'm Little Endian\n");
+    else
+        printf("I'm not Little Endian\n");
 
-	if (IsLittleEndian())
-		printf("I'm Little Endian\n");
-	if (IsBigEndian())
-		printf("I'm Big Endian\n");
-	WSACleanup();
+    if (IsBigEndian())
+        printf("I'm Big Endian\n");
+    else
+        printf("I'm not Big Endian\n");
 }
