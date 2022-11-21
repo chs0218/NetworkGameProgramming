f7a9da17313da80e9ac2170e122f3c0bd12f8164
Choi Hae Seong <2018180044@tukorea.ac.kr>
2022-11-21 17:22:48 +0900

    통신

--- WindowApp/7_1_Client/7_1_Client.cpp
+++ WindowApp/7_1_Client/7_1_Client.cpp
@@ -1,11 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS // 구형 C 함수 사용 시 경고 끄기
-#include <windows.h>
-#include <tchar.h>
-#include <stdio.h>
-#include <shobjidl.h> 
+//#include <windows.h>
+//#include <tchar.h>
+//#include <stdio.h>
+#include "..\..\Common.h"
 #include "resource.h"
+#include <shobjidl.h>
+#include <fstream>
 
-#define BUFSIZE 25
+char* SERVERIP = (char*)"127.0.0.1";
+#define SERVERPORT 9000
+#define BUFSIZE 512
+
+#pragma pack(1)
+struct DataSet {
+	int len = 0;	// 데이터 길이
+	char name[50] = "\0";	// 데이터 파일 이름
+	char buf[BUFSIZE + 1];	// 데이터 버퍼
+};
+#pragma pack()
 
 // 대화상자 프로시저
 INT_PTR CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
@@ -13,16 +25,25 @@ INT_PTR CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
 void DisplayText(const char* fmt, ...);
 void DisplayFileName();
 HANDLE hReadEvent, hWriteEvent; // 이벤트
-HWND hSendButton; // 보내기 버튼
+HWND hSelectButton; // 보내기 버튼
 HWND hEdit1, hEdit2; // 에디트 컨트롤
+HWND hWnd;
+
+// 데이터 통신에 사용할 변수
+DataSet data;
+char pFilePath[500];
 
 // 소켓 통신 스레드 함수
 DWORD WINAPI ClientMain(LPVOID arg);
-char buf[BUFSIZE + 1]; // 임시 데이터 송수신 버퍼
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	LPSTR lpCmdLine, int nCmdShow)
 {
+	// 윈속 초기화
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		return 1;
+
 	// 이벤트 생성
 	hReadEvent = CreateEvent(NULL, FALSE, TRUE, NULL);
 	hWriteEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
@@ -36,29 +57,31 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	// 이벤트 제거
 	CloseHandle(hReadEvent);
 	CloseHandle(hWriteEvent);
+
+	// 윈속 종료
+	WSACleanup();
 	return 0;
 }
 
 // 대화상자 프로시저
 INT_PTR CALLBACK DlgProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	static char buf[BUFSIZE + 1];
 	switch (uMsg) {
 	case WM_INITDIALOG:
 		hEdit1 = GetDlgItem(hDlg, IDC_EDIT1);
-		hSendButton = GetDlgItem(hDlg, IDSELECT);
+		hSelectButton = GetDlgItem(hDlg, IDSELECT);
 		return TRUE;
 	case WM_COMMAND:
 		switch (LOWORD(wParam)) {
 		case IDSELECT:
-			EnableWindow(hSendButton, FALSE); // 보내기 버튼 비활성화
-			WaitForSingleObject(hReadEvent, INFINITE); // 읽기 완료 대기
 			DisplayFileName();
-			SetEvent(hWriteEvent); // 쓰기 완료 알림
 			SetFocus(hEdit1); // 키보드 포커스 전환
 			SendMessage(hEdit1, EM_SETLIMITTEXT, BUFSIZE, 0);
 			return TRUE;
 		case IDOK:
+			WaitForSingleObject(hReadEvent, INFINITE); // 읽기 완료 대기
+			EnableWindow(hSelectButton, FALSE); // 보내기 버튼 비활성화
+			SetEvent(hWriteEvent); // 쓰기 완료 알림				
 			return TRUE;
 		case IDCANCEL:
 			EndDialog(hDlg, IDCANCEL);
@@ -78,65 +101,100 @@ void DisplayText(const char* fmt, ...)
 	vsprintf(cbuf, fmt, arg);
 	va_end(arg);
 
-	int nLength = GetWindowTextLength(hEdit2);
-	SendMessage(hEdit2, EM_SETSEL, nLength, nLength);
-	SendMessageA(hEdit2, EM_REPLACESEL, FALSE, (LPARAM)cbuf);
+	int nLength = GetWindowTextLength(hEdit1);
+	SendMessage(hEdit1, EM_SETSEL, nLength, nLength);
+	SendMessageA(hEdit1, EM_REPLACESEL, FALSE, (LPARAM)cbuf);
 }
 
 void DisplayFileName()
 {
-	HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
-	if (SUCCEEDED(hr))
+	OPENFILENAME ofn;       // common dialog box structure
+	TCHAR szFile[260] = { 0 };       // if using TCHAR macros
+
+	// Initialize OPENFILENAME
+	ZeroMemory(&ofn, sizeof(ofn));
+	ofn.lStructSize = sizeof(ofn);
+	ofn.hwndOwner = hWnd;
+	ofn.lpstrFile = szFile;
+	ofn.nMaxFile = sizeof(szFile);
+	ofn.lpstrFilter = _T("All\0*.*\0Text\0*.TXT\0");
+	ofn.nFilterIndex = 1;
+	ofn.lpstrFileTitle = NULL;
+	ofn.nMaxFileTitle = 0;
+	ofn.lpstrInitialDir = NULL;
+	ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+
+	if (GetOpenFileName(&ofn) == TRUE)
 	{
-		IFileOpenDialog* pFileOpen;
+		// use ofn.lpstrFile
+		wcstombs(pFilePath, ofn.lpstrFile, 500);
+		SetWindowText(hEdit1, L"");
 
-		// Create the FileOpenDialog object.
-		hr = CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_ALL, IID_IFileOpenDialog, reinterpret_cast<void**>(&pFileOpen));
+		char* FileName = strrchr(pFilePath, '\\');
+		(FileName) ? strcpy(data.name, FileName + 1) : strcpy(data.name, pFilePath);
 
-		if (SUCCEEDED(hr))
-		{
-			// Show the Open dialog box.
-			hr = pFileOpen->Show(NULL);
-
-			// Get the file name from the dialog box.
-			if (SUCCEEDED(hr))
-			{
-				IShellItem* pItem;
-				hr = pFileOpen->GetResult(&pItem);
-				if (SUCCEEDED(hr))
-				{
-					PWSTR pszFilePath;
-					hr = pItem->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
-
-					// Display the file name to the user.
-					if (SUCCEEDED(hr))
-					{
-						MessageBoxW(NULL, pszFilePath, L"File Path", MB_OK);
-						CoTaskMemFree(pszFilePath);
-					}
-					pItem->Release();
-				}
-			}
-			pFileOpen->Release();
-		}
-		CoUninitialize();
+		DisplayText("%s\r\n", pFilePath);
 	}
 }
 
 // TCP 클라이언트 시작 부분
 DWORD WINAPI ClientMain(LPVOID arg)
 {
+	int retval;
+
 	while (1) {
 		WaitForSingleObject(hWriteEvent, INFINITE); // 쓰기 완료 대기
 
-		// 문자열 길이가 0이면 보내지 않음
-		if (strlen(buf) == 0) {
-			EnableWindow(hSendButton, TRUE); // 보내기 버튼 활성화
-			SetEvent(hReadEvent); // 읽기 완료 알림
-			continue;
+		// 소켓 생성
+		SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+		if (sock == INVALID_SOCKET) err_quit("socket()");
+
+		// connect()
+		struct sockaddr_in serveraddr;
+		memset(&serveraddr, 0, sizeof(serveraddr));
+		serveraddr.sin_family = AF_INET;
+		inet_pton(AF_INET, SERVERIP, &serveraddr.sin_addr);
+		serveraddr.sin_port = htons(SERVERPORT);
+		retval = connect(sock, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+		if (retval == SOCKET_ERROR) err_quit("connect()");
+
+		std::ifstream in;
+
+		in.open(pFilePath, std::ios::binary);
+
+		in.seekg(0, std::ios::end);
+		data.len = in.tellg();
+		in.seekg(0, std::ios::beg);
+
+		retval = send(sock, (char*)&data.len, sizeof(int), 0);
+		if (retval == SOCKET_ERROR) {
+			err_display("send()");
 		}
 
-		EnableWindow(hSendButton, TRUE); // 보내기 버튼 활성화
+		retval = send(sock, (char*)&data.name, 50, 0);
+		if (retval == SOCKET_ERROR) {
+			err_display("send()");
+		}
+
+		while (in.read(data.buf, BUFSIZE))
+		{
+			retval = send(sock, data.buf, BUFSIZE, 0);
+			if (retval == SOCKET_ERROR) {
+				err_display("send()");
+				break;
+			}
+		}
+
+		retval = send(sock, data.buf, data.len % BUFSIZE, 0);
+		if (retval == SOCKET_ERROR) {
+			err_display("send()");
+		}
+
+		EnableWindow(hSelectButton, TRUE); // 보내기 버튼 활성화
+
+		in.close();
+
+		closesocket(sock); // 소켓 닫기
 		SetEvent(hReadEvent); // 읽기 완료 알림
 	}
 
